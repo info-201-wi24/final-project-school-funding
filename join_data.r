a2d9083dd4b79dcf214e4008369833b5675a7d95
Leah Jia <99011487+leahjia@users.noreply.github.com>
2024-02-24 19:20:30 -0800

    variables & df

--- join_data.r
+++ join_data.r
@@ -15,6 +15,31 @@ df_demo_summarized <- df_demo %>%
 result <- df_funding_summarized %>%
   inner_join(df_demo_summarized, by = c("STATE" = "stname"))
 
+# categorical var
+quantiles <- quantile(result$TFEDREV / result$TOTALREV, probs = c(0.33, 0.66))
+lower <- quantiles[1]
+upper <- quantiles[2]
+
+result <- result %>%
+  mutate(funding_level = case_when(
+    TFEDREV / TOTALREV < lower ~ "Low",
+    TFEDREV / TOTALREV >= lower & TFEDREV / TOTALREV <= upper ~ "Medium",
+    TFEDREV / TOTALREV > upper ~ "High"
+  ))
+
+# numerical
+result <- result %>%
+  mutate(male_female_ratio = tot_male / tot_female)
+
+# summary
+summary_df <- result %>%
+  group_by(funding_level) %>%
+  summarise(
+    avg_male_female_ratio = mean(male_female_ratio, na.rm = TRUE),
+    median_total_rev = median(TOTALREV, na.rm = TRUE),
+    avg_federal_funding = mean(TFEDREV, na.rm = TRUE)
+  )
+
 # check size before writing to file
 if (nrow(result) < 20000) {
   write.csv(result, "joined_ny_data.csv", row.names = FALSE)
